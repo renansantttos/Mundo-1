417c04a824c775a4f8bd006d104542c67c9846ff
renansantttos <renansantttos@gmail.com>
2024-04-22 00:11:49 -0300

    Quinto desafio

---
+++ Desafio005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))
+n2 = n + 1
+n3 = n - 1
+print('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}'.format(n, n3, n2))
