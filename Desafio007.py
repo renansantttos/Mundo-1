fc9ac5aea4616d4a0161213af21414860adf44d9
renansantttos <renansantttos@gmail.com>
2024-04-22 00:23:09 -0300

    Setimo desafio

---
+++ Desafio007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1 + n2) / 2
+print('A média entre {} e {} é igual a {:.1f}'.format(n1, n2, media))
