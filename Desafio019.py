87dff56d8c9865f271f8b0ec4242ade3a2b89a4c
renansantttos <renansantttos@gmail.com>
2024-04-22 20:45:05 -0300

    Decimo nono desafio

---
+++ Desafio019.py
@@ -0,0 +1,13 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+
+x = [a1,a2,a3,a4]
+
+sorteio = random.choice(x)
+
+print('O aluno sorteado foi {}'.format(sorteio))
