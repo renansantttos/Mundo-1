2ab1d3c1f4d4e3619b4f6ad8a62b38994ca61a7d
renansantttos <renansantttos@gmail.com>
2024-04-22 00:33:47 -0300

    Nono desafio

---
+++ Desafio009.py
@@ -0,0 +1,14 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+n = int(input('Digite um número para descobrir a sua taboada: '))
+n1 = n * 1
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print('A tabuada do número escolhido é:\n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {} \n {} x {} = {}\n {} x {} = {}'.format(n,1,n1,n,2,n2,n,3,n3,n,4,n4,n,5,n5,n,6,n6,n,7,n7,n,8,n8,n,9,n9,n,10,n10))
